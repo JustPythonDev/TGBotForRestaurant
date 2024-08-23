ae3d89b13437f10710d95544d4e589f2b34e06e3
sabir116rus <sabir116rus@gmail.com>
2024-08-23 11:35:17 +0300

    Add new table menu_items and populated it

--- creation_table.py
+++ creation_table.py
@@ -67,5 +67,37 @@ cur.execute("""
     )
 """)
 
+# Создаем таблицу menu_items
+cur.execute("""
+CREATE TABLE menu_items (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    text TEXT NOT NULL,
+    image_url TEXT,
+    callback TEXT UNIQUE NOT NULL,
+    parent_menu TEXT,
+    order_by INTEGER NOT NULL,
+    FOREIGN KEY (parent_menu) REFERENCES menu_items(callback)
+);
+""")
+
+# Вставляем данные в таблицу menu_items
+cur.executemany("""
+INSERT INTO menu_items (name, text, callback, parent_menu, order_by, image_url)
+VALUES (?, ?, ?, ?, ?, ?)
+""", [
+    ('Меню кафе', 'Выберите категорию блюда', 'menu', None, 1, ''),
+    ('Корзина', 'Выбранные вами блюда', 'cart', None, 2, ''),
+    ('Оплата заказа', 'Выберите способ оплаты', 'payment', None, 3, ''),
+    ('Статус заказа', 'Выберите заказ для просмотра', 'status', None, 4, ''),
+    ('Отзывы', 'Выберите категорию отзыва', 'feedback', None, 5, ''),
+    ('Закуски', 'Выберите закуску', 'appetizers', 'menu', 1, 'img/appetizers.jpg'),
+    ('Салаты', 'Выберите салат', 'salads', 'menu', 2, 'img/salads.jpg'),
+    ('Первые блюда', 'Выберите суп', 'soups', 'menu', 3, 'img/soups.jpg'),
+    ('Основные блюда', 'Выберите горячее', 'main_dishes', 'menu', 4, 'img/main_dishes.jpg'),
+    ('Десерты', 'Выберите десерт', 'desserts', 'menu', 5, '')
+])
+
+# Сохраняем изменения
 conn.commit()
 conn.close()

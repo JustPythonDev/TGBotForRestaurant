4cfea3d4b7c12715055c47e44eeac2a17f23475a
Julia <JustPythonDev@ya.ru>
2024-08-25 00:19:53 +0300

    bot 8

--- bot.py
+++ bot.py
@@ -6,50 +6,24 @@ import os
 
 class Menu:
     def __init__(self):
-        # Определение структуры меню
-        # self.menu_structure = [
-        #     {"name": "Меню кафе", "text": "Выберите категорию блюда", "callback": "menu", "parent_menu": None, "order_by": 1,
-        #      "image_url": ""},
-        #     {"name": "Корзина", "text": "Выбранные вами блюда", "callback": "cart", "parent_menu": None, "order_by": 2,
-        #      "image_url": ""},
-        #     {"name": "Оплата заказа", "text": "Выберите способ оплаты", "callback": "payment", "parent_menu": None,
-        #      "order_by": 3, "image_url": ""},
-        #     {"name": "Статус заказа", "text": "Выберите заказ для просмотра", "callback": "status", "parent_menu": None,
-        #      "order_by": 4, "image_url": ""},
-        #     {"name": "Отзывы", "text": "Выберите категорию отзыва", "callback": "feedback", "parent_menu": None,
-        #      "order_by": 5, "image_url": ""},
-        #     {"name": "Закуски", "text": "Выберите закуску", "callback": "appetizers", "parent_menu": "menu",
-        #      "order_by": 1, "image_url": "img/appetizers.jpg"},
-        #     {"name": "Салаты", "text": "Выберите салат", "callback": "salads", "parent_menu": "menu", "order_by": 2,
-        #      "image_url": "img/salads.jpg"},
-        #     {"name": "Первые блюда", "text": "Выберите суп", "callback": "soups", "parent_menu": "menu", "order_by": 3,
-        #      "image_url": "img/soups.jpg"},
-        #     {"name": "Основные блюда", "text": "Выберите горячее", "callback": "main_dishes", "parent_menu": "menu",
-        #      "order_by": 4, "image_url": "img/main_dishes.jpg"},
-        #     {"name": "Десерты", "text": "Выберите десерт", "callback": "desserts", "parent_menu": "menu", "order_by": 5,
-        #      "image_url": ""}
-        # ]
-        # self.user_states = {}  # Словарь для хранения состояния пользователя
-
-
         # Создаем экземпляр базы данных и получаем сессию
         self.db = Database()
         self.session = self.db.get_session()
-
+        self.last_menu = ""
 
     def create_menu_keyboard(self, parent_callback):
         # Создание клавиатуры для текущего уровня меню
         markup = types.InlineKeyboardMarkup()
 
         # Получение элементов меню из базы данных
-        filtred_menu = MenuItem.get_menu_items_by_parent(self.session, parent_callback)
+        filtered_menu = MenuItem.get_menu_items_by_parent(self.session, parent_callback)
 
-        for item in filtred_menu:
+        for item in filtered_menu:
             button = types.InlineKeyboardButton(item["name"], callback_data=item["callback"])
             markup.add(button)
 
         # Если это не главное меню, добавляем кнопку для возврата
-        if parent_callback is not None:
+        if parent_callback is not None and parent_callback != "start":
             parent_of_parent = self.get_parent_menu_callback(parent_callback)
             if parent_of_parent is None:
                 parent_of_parent = "start"
@@ -67,7 +41,6 @@ class Menu:
 
     def get_image_url(self, callback):
         # Получение URL изображения для меню на основе callback
-
         item = MenuItem.get_menu_item_data(self.session, callback)
         if item:
             return item['image_url']
@@ -88,82 +61,87 @@ bot = telebot.TeleBot(TELEGRAM_API_TOKEN)
 menu = Menu()
 
 
-# Обработчик команды /start
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    # Отправка основного меню
-    main_menu = menu.create_menu_keyboard(None)
-    with open('img/main_photo.jpg', 'rb') as photo:
-        bot.send_photo(message.chat.id, photo,
-                                    caption="Добро пожаловать в наше кафе! Выберите действие:",
-                                    reply_markup=main_menu)
-    # Удаление предыдущего сообщения
-    if message.text:
-        bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-
-
-# Обработчик нажатий на inline-кнопки
-@bot.callback_query_handler(func=lambda call: True)
-def handle_callback(call):
-    msg = call.message
-    user_id = msg.chat.id
-
-    # if call.data == "back_to_start":
-    #     send_welcome(msg)  # Возвращаемся на стартовое меню
-    #     return
-
-    if call.data.startswith("back_to_"):
-        parent_callback = call.data[len("back_to_"):]
-
+def process_menu(user_id, parent_callback, msg=None):
+    # Создание клавиатуры и получение текста и изображения
+    if parent_callback == menu.last_menu:
+        return
     else:
-        parent_callback = call.data
+        menu.last_menu = parent_callback
 
     menu_keys = menu.create_menu_keyboard(parent_callback)
     menu_text = menu.get_menu_text(parent_callback)
     image_url = menu.get_image_url(parent_callback)
 
+    # Проверяем, если image_url указан и файл существует
     if image_url and os.path.isfile(image_url):
         image_media = types.InputMediaPhoto(media=open(image_url, 'rb'), caption=menu_text)
     else:
         image_media = None
 
     # Если есть изображение в сообщении и есть изображение для меню - редактируем изображение
-    if msg.photo:
-        if image_media:
-            bot.edit_message_media(
-                media=image_media,
-                chat_id=user_id,
-                message_id=msg.message_id,
-                reply_markup=menu_keys
-            )
+    if msg:
+        if msg.photo:
+            if image_media:
+                bot.edit_message_media(
+                    media=image_media,
+                    chat_id=user_id,
+                    message_id=msg.message_id,
+                    reply_markup=menu_keys
+                )
+            elif msg.text:
+                bot.edit_message_text(text=menu_text,
+                                      chat_id=user_id,
+                                      message_id=msg.message_id,
+                                      reply_markup=menu_keys)
+            else:
+                bot.send_message(user_id, text=menu_text, reply_markup=menu_keys)
+
+           # bot.delete_message(chat_id=user_id, message_id=msg.message_id)
+        elif image_media:
+            with open(image_url, 'rb') as photo:
+                bot.send_photo(
+                    user_id,
+                    image_media.media,
+                    caption=menu_text,
+                    reply_markup=menu_keys
+                )
+
+            if msg.text:
+                bot.delete_message(chat_id=user_id, message_id=msg.message_id)
         elif msg.text:
+            # Если картинки нет, просто редактируем текст
             bot.edit_message_text(text=menu_text,
-                              chat_id=user_id,
-                              message_id=msg.message_id,
-                              reply_markup=menu_keys)
+                                  chat_id=user_id,
+                                  message_id=msg.message_id,
+                                  reply_markup=menu_keys)
         else:
             bot.send_message(user_id, text=menu_text, reply_markup=menu_keys)
-
-        # bot.delete_message(chat_id=user_id, message_id=msg.message_id)
-    elif image_media:
-        with open(image_url, 'rb') as photo:
+    else:
+        if image_media:
             bot.send_photo(
                 user_id,
                 image_media.media,
                 caption=menu_text,
                 reply_markup=menu_keys
             )
+        else:
+            bot.send_message(user_id, text=menu_text, reply_markup=menu_keys)
 
-        if msg.text:
-            bot.delete_message(chat_id=user_id, message_id=msg.message_id)
-    elif msg.text:
-        # Если картинки нет, просто редактируем текст
-        bot.edit_message_text(text=menu_text,
-                              chat_id=user_id,
-                              message_id=msg.message_id,
-                              reply_markup=menu_keys)
-    else:
-        bot.send_message(user_id, text=menu_text, reply_markup=menu_keys)
+
+# Обработчик команды /start
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    user_id = message.chat.id
+    process_menu(user_id, "start", msg=message)
+
+
+# Обработчик нажатий на inline-кнопки
+@bot.callback_query_handler(func=lambda call: True)
+def handle_callback(call):
+    user_id = call.message.chat.id
+    parent_callback = call.data[len("back_to_"): ] if call.data.startswith("back_to_") else call.data
+    # Вызываем process_menu для обработки inline-кнопок
+    process_menu(user_id, parent_callback, msg=call.message)
 
 
 bot.polling()

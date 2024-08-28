a2ece7cf607e780b2de4a482c4764a2f085c268e
Meandr58 <andrey.mescherin@gmail.com>
2024-08-28 22:28:20 +0300

    reviews

---
+++ status.py
@@ -0,0 +1,14 @@
+from database import Orders, Base
+
+def check_order_statuses(user_id):
+
+    orders = get_orders_by_user_id(user_id=user_id)
+
+    if orders:
+        status_message = ""
+        for order in orders:
+            status_message += f"Ваш заказ {order.id} на сумму {order.total_amount} {order.status}\n\n\n"
+    else:
+        status_message = "У вас нет активных заказов на сегодня."
+
+    return status_message
